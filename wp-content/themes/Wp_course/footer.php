176a5341d03bad9056ed2d3c6ec5c521610f9f1a
Eugene Shalko <eugene.shalko.00@gmail.com>
2020-11-18 17:42:25 +0200

    Issue
    -change index.php
    change footer.php
    -change normalize.css and style.css

--- wp-content/themes/Wp_course/footer.php
+++ wp-content/themes/Wp_course/footer.php
@@ -1,4 +1,7 @@
-	<footer id="colophon" class="site-footer">
+<div class="clean"></div>
+<hr>
+
+<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp_course' ) ); ?>">
 				<?php
--- wp-content/themes/Wp_course/index.php
+++ wp-content/themes/Wp_course/index.php
@@ -1,35 +1,27 @@
 <?php get_header(); ?>
 
 	<main id="primary" class="site-main">
-
+    <div class="posts">
 		<?php
 
 		if ( have_posts() ) :
-            get_sidebar();
-
-            if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
 			while ( have_posts() ) :
 				the_post();
 				get_template_part( 'template-parts/content', get_post_type() );
+            ?>
+                <hr>
+                <div class="clean"></div>
 
+            <?php
 			endwhile;
-
-			the_posts_navigation();
-
 		else :
-
 			get_template_part( 'template-parts/content', 'none' );
-
 		endif;
 		?>
-        <div class="clean"></div>
+    </div>
+
 	</main><!-- #main -->
+<?php get_sidebar();?>
     <div class="delimer"></div>
 <?php
 get_footer();
