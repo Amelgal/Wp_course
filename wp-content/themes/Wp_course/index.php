2618fc61b0eb8b73a1270ee77ea933c80275bcbd
Eugene Shalko <eugene.shalko.00@gmail.com>
2020-11-23 15:31:50 +0200

    Issue#7
    -add feature slider in main page
    -using shortcode
    -change css style

--- wp-content/themes/Wp_course/index.php
+++ wp-content/themes/Wp_course/index.php
@@ -1,27 +1,28 @@
 <?php get_header(); ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary_page" class="site-main">
     <div class="posts">
-		<?php
+        <?php
+            do_shortcode('[slider numberposts = 3 post_type = swim]');
+        ?>
 
-		if ( have_posts() ) :
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'template-parts/content', get_post_type() );
-            ?>
-                <hr>
-                <div class="clean"></div>
-
-            <?php
-			endwhile;
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
-		?>
+<?php
+//
+//		if ( have_posts() ) :
+//			while ( have_posts() ) :
+//				the_post();
+//				get_template_part( 'template-parts/content', get_post_type() );
+//            ?>
+<!--                <hr>-->
+<!--                <div class="clean"></div>-->
+<?php //			endwhile;
+//		else :
+//			get_template_part( 'template-parts/content', 'none' );
+//		endif;
+//         ?>
     </div>
 
 	</main><!-- #main -->
-<?php get_sidebar();?>
     <div class="delimer"></div>
 <?php
 get_footer();
