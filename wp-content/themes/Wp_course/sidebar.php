d765d5d3271a709094cf730040c5c5b4fb78a416
Eugene Shalko <eugene.shalko.00@gmail.com>
2020-11-24 23:05:37 +0200

    Issue #9
    -fix code style using the IDE

--- wp-content/themes/Wp_course/sidebar.php
+++ wp-content/themes/Wp_course/sidebar.php
@@ -9,12 +9,12 @@
 <!--</aside> #secondary -->
 
 <div class="sidebar">
-    <h2><?php _e('Categories');?></h2>
+    <h2><?php _e('Categories'); ?></h2>
     <ul>
-        <?php wp_list_categories('sort_column=name&optioncount=1&hierarchial=0');?>
+        <?php wp_list_categories('sort_column=name&optioncount=1&hierarchial=0'); ?>
     </ul>
-    <h2><?php _e('Archives');?></h2>
+    <h2><?php _e('Archives'); ?></h2>
     <ul>
-        <?php wp_get_archives('type=monthly');?>
+        <?php wp_get_archives('type=monthly'); ?>
     </ul>
 </div>
